e149acc892d221cfc2793bcad9f25538c993dafc
Leandro Félix <58048297+lehinfo-felix@users.noreply.github.com>
2023-02-16 14:51:54 -0300

    fix: CORS

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 /* eslint-disable import/first */
+
+
 import express from 'express'
+const cors = require('cors')
 import router from './routes/router'
 
-const app = express()
 const port = process.env.PORT
+const app = express()
 
 app.use(express.json())
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE')
+    app.use(cors())
+    next()
+});
 app.use('/api/', router)
 
 app.listen(port, async () => {
